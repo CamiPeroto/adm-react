1109baa5ba7b8e81112b2d5312a77503973426f3
CamiPeroto <camilabuenoperoto@gmail.com>
2025-04-26 10:30:14 -0300

    Enviar o token para a API na requisição

--- src/service/api.tsx
+++ src/service/api.tsx
@@ -1,10 +1,29 @@
 import axios, { AxiosInstance } from "axios";
 
+
 const instance: AxiosInstance = axios.create({
     baseURL: "http://localhost:8080", //definir a URL base para todas as requisições
     headers: {
         "Content-Type": "application/json", //definir cabeçalho padrão para envio de dados no formato json
     }
 });
+//interceptador para adicionar o token automaticamente nas requisições
+instance.interceptors.request.use(
+    (config) => {
+        //Verifica se está no cliente antes de acessar o localStorage
+        if(typeof window !== "undefined"){
+            const token = localStorage.getItem("token");
+
+            if(token){
+                config.headers.Authorization = `Bearer ${token}`;
+                console.log(`Bearer ${token}`);
+            }
+        }
+        return config;
+        
+}, (error) =>{
+    return Promise.reject(error);
+}
+);
 
 export default instance;
