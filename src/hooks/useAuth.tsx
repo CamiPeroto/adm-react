0bd3d27eed51e75f7b815b7976fda17a63a9412d
CamiPeroto <camilabuenoperoto@gmail.com>
2025-04-26 10:45:13 -0300

    Validar o token antes de carregar a página

--- src/hooks/useAuth.tsx
+++ src/hooks/useAuth.tsx
@@ -1,6 +1,7 @@
 //hook pra manipular navegação do usuário
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
+import instance from "@/service/api";
 
 export function useAuth(){
     
@@ -9,6 +10,9 @@ export function useAuth(){
 
     //estado para armazenar a autenticação
     const[ authenticated, setAuthenticated ] = useState<boolean>(false);
+   
+    //Apresentar carregamento
+    const [loading, setLoading] = useState<boolean>(true);
     
     //hook para verificar se o token existe
     useEffect(()=>{
@@ -19,12 +23,31 @@ export function useAuth(){
         if(!token){
             //redireciona para o login se não estiver autenticado
             router.push("/login");
-        }else{
-            //atribui a situação da autenticação
-            setAuthenticated(true);
         }
+            //função para validar o token
+            const validateToken = async() => {
+                try{
+                    //fazer requisição para a API
+                    await instance.get("/validate-token");
+                     
+                    //atribui a situação da autenticação
+                     setAuthenticated(true);
+
+                }catch(error){
+                    //remover token inválido
+                    localStorage.removeItem("token");
+                    
+                    //redireciona para o login se o token for inválido
+                    router.push("/login");
+                }  finally{
+                    setLoading(false);
+                }
+        }
+        //chamar a função
+        validateToken();
+
     }, []); //colchete para executar somente uma vez
     
     //retornar a situação da autenticação
-    return { authenticated };
+    return { authenticated, loading };
 }
